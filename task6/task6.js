b7fba3c8313e98c8a2f130b395209153ff951c85
2025274192rishabh-ctrl <2025274192.rishabh@ug.sharda.ac.in>
2025-11-26 22:41:41 +0530

    Create task6.js

---
+++ task6/task6.js
@@ -0,0 +1,10 @@
+// Creating two variables
+let a = 5;
+let b = 10;
+
+// Calculating sum
+let sum = a + b;
+
+// Updating the paragraph content
+document.getElementById("result").innerHTML =
+    "The sum of " + a + " and " + b + " is " + sum;
